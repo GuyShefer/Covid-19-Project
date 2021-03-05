a2bf0c14a6d5174750aab3b1f572c84704fd3c00
Guy <shefer.guy1@gmail.com>
2021-03-05 22:25:13 +0200

    .

--- script/index.js
+++ script/index.js
@@ -6,6 +6,8 @@
     let currentRegion = 'World';
     const indicatorBtns = document.querySelectorAll('.ind-btns');
     const regionBtns = document.querySelectorAll('.reg-btn');
+    const dropDown = document.querySelector('.drop-down');
+    let select = document.querySelector('#selection');
     getWorld();
 
     async function getWorld() {
@@ -20,37 +22,47 @@
     async function getCountryByCCA(code) {
         if (code !== 'XK') {
             const response = await fetch(`${coronaBaseUrl}${code}`);
-            const data = await response.json();
-            let country;
-            country = {
-                name: data.data.name,
-                confirmed: data.data.latest_data.confirmed,
-                deaths: data.data.latest_data.deaths,
-                recovered: data.data.latest_data.recovered,
-                critical: data.data.latest_data.critical,
+            const data = (await response.json()).data;
+            const country = {
+                name: data.name,
+                confirmed: data.latest_data.confirmed,
+                deaths: data.latest_data.deaths,
+                recovered: data.latest_data.recovered,
+                critical: data.latest_data.critical,
+                code: code,
             }
             return country;
         }
     }
 
-    // convert the world object to 2 arrays for diplay in the chart.
+    // convert the world object to 2 arrays(country name ,indicator value) to diplay it in the chart.
     function convetObjToArrays(indicatorBtn) {
+        if (select !== null) {
+            select.remove();
+        }
+        select = document.createElement('select');
+        dropDown.appendChild(select);
         const labelsArr = [];
         const dataArr = [];
 
         if (currentRegion === 'World') {
             Object.keys(world).forEach(region => {
-                addRegionIndicatorToArrays(region);
+                addRegionIndicToArraysAndAddDropDownOption(region);
             })
         } else {
-            addRegionIndicatorToArrays(currentRegion);
+            addRegionIndicToArraysAndAddDropDownOption(currentRegion);
         }
 
-        function addRegionIndicatorToArrays(currReg) {
-            world[currReg].forEach(city => {
-                if (city !== undefined) {
-                    labelsArr.push(city.name);
-                    dataArr.push(city[indicatorBtn]);
+        function addRegionIndicToArraysAndAddDropDownOption(currReg) {
+            world[currReg].forEach(country => {
+                if (country !== undefined) {
+                    labelsArr.push(country.name);
+                    dataArr.push(country[indicatorBtn]);
+                    // adding new option to the dropdown.
+                    const option = document.createElement('option');
+                    option.value = country.code;
+                    option.innerHTML += `${country.name}`;
+                    select.appendChild(option);
                 }
             })
         }
@@ -99,6 +111,69 @@
             convetObjToArrays('confirmed');
         });
     }
+
+    const showCountyBtn = document.querySelector('.select-btn');
+    showCountyBtn.addEventListener('click', () => {
+        getCountryInfo(); // should rename to open modal
+        modal.style.display = "block";
+    })
+
+    // duplicate function!@#
+    const getCountryInfo = async () => {
+        const cca = select.options[select.selectedIndex].value;
+        const response = await fetch(`${coronaBaseUrl}${cca}`);
+        const data = (await response.json()).data;
+        // getting the country obj from the api
+        const country = {
+            population: data.population,
+            deaths: data.latest_data.deaths,
+            confirmed: data.latest_data.confirmed,
+            critical: data.latest_data.critical,
+            recovered: data.latest_data.recovered,
+            deathRate: (data.latest_data.calculated.death_rate).toFixed(2),
+        }
+        modalHeader.textContent = (data.name).toUpperCase(); // set modal title.
+
+
+        if(modalBody){
+            modalBody.remove();
+            modalBody = document.createElement('div')
+            modalBody.classList.add("modal-body");
+            modal.children[0].appendChild(modalBody);
+        }
+
+        for (let [key, value] of Object.entries(country)) {
+            const box = document.createElement('div');
+            const boxHeader = document.createElement('h4');
+            const boxData = document.createElement('p');
+            box.classList.add('box');
+            boxHeader.textContent = key;
+            boxData.textContent = value;
+            box.appendChild(boxHeader);
+            box.appendChild(boxData);
+            modalBody.appendChild(box);
+
+        }
+
+
+    }
+
+    const modalHeader = document.querySelector('h3');
+    let modalBody = document.querySelector('.modal-body');
+
+    const modal = document.querySelector('.modal');
+    const closeModal = document.querySelector('.close');
+    closeModal.addEventListener('click', () => {
+        modal.style.display = "none";
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
 
 })();
 
